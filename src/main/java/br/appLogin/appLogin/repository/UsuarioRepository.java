4d9518dae4862837380b1bc2d315db926cd733b7
LuizRibeir0 <luizfeliperds13@gmail.com>
2025-08-12 00:01:27 -0300

    criação do UsuarioDTO, e correções em linhas do codigo

--- src/main/java/br/appLogin/appLogin/repository/UsuarioRepository.java
+++ src/main/java/br/appLogin/appLogin/repository/UsuarioRepository.java
@@ -1,10 +1,8 @@
 package br.appLogin.appLogin.repository;
 
 import br.appLogin.appLogin.model.Usuario;
-import br.appLogin.appLogin.model.UsuarioDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +13,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario,String> {
 
 
     Usuario findById(long id);
-    Usuario deleteUsuarioById(long id);
 
     @Query(value = "select * from applogin.usuario where email = :email and senha = :senha", nativeQuery = true)
     public Usuario login(String email, String senha);
