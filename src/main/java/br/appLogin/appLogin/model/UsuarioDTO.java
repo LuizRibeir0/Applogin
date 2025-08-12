0057f70822308aa46ece7a70885e7cb8e6ded789
LuizRibeir0 <luizfeliperds13@gmail.com>
2025-08-11 23:49:24 -0300

    criação do UsuarioDTO.

---
+++ src/main/java/br/appLogin/appLogin/model/UsuarioDTO.java
@@ -0,0 +1,43 @@
+package br.appLogin.appLogin.model;
+
+public class UsuarioDTO {
+
+    private long Id;
+    private String nome;
+    private String email;
+
+    public UsuarioDTO () {}
+
+    public UsuarioDTO(Usuario usuario) {
+       this.Id = usuario.getId();
+       this.nome = usuario.getNome();
+       this.email = usuario.getEmail();
+    }
+
+    public UsuarioDTO(UsuarioDTO usuarioDTO) {
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
